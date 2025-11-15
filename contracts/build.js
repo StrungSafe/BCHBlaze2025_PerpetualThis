4234a2dc91ad1a6513b1a1e8c1a3662053a7f472
Miguel Molina Jr. <molim134@gmail.com>
2025-11-15 11:54:06 -0500

    Add cashscript package

--- contracts/build.js
+++ contracts/build.js
@@ -1,8 +1,8 @@
 import { compileFile } from 'cashc';
 import { writeFile } from 'fs';
 
-const artifact = compileFile(new URL('perpetual.cashc', import.meta.url));
-writeFile('src/artifacts/perpetual.artc', JSON.stringify(artifact), error => {
+const artifact = compileFile(new URL('perpetual.cash', import.meta.url));
+writeFile('src/art/perpetual.cashc', JSON.stringify(artifact), error => {
     if(error) {
         throw error;
     }
